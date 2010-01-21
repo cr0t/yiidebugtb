ac2d1fa8ae504930fc166b530c462068e013bfb9
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2010-01-22 01:49:14 +0300

    Initial GIT commit

---
+++ XWebDebugRouter.php
@@ -0,0 +1,476 @@
+<?php
+
+/*
+ * Yii Debug Toolbar
+ *
+ * Using:
+ * 1. First Step.
+ * Extract yiidebugtb folder to [webroot]/protected/extensions/yiidebugtb
+ * --Test:
+ * -- You must have an "[webroot]/protected/extensions/yiidebugtb/XWebDebugRouter.php" file
+ * -- if you all right way.
+ *
+ * 2. Second Step.
+ * Open [webroot]/protected/config/main.php, find section 'import' and add following lines which belongs to yiidebugtb:
+ *
+ * [...]
+ * // autoloading model and component classes
+ * 'import'=>array(
+ * 		'application.models.*',
+ * 		'application.extensions.yiidebugtb.*'
+ * 		[...]
+ * ),
+ * [...]
+ *
+ * 3. And Last Step.
+ * In [webroot]/protected/config/main.php find section 'routes' and add following lines for XWebDebugRouter:
+ *
+ * [...]
+ * 'routes'=>array(
+ * 		array(
+ * 			'class'=>'XWebDebugRouter',
+ * 			'config'=>'alignLeft, opaque, runInDebug, fixedPos, collapsed',
+ * 			'levels'=>'error, warning, trace, profile, info'
+ * 		),
+ * ),
+ * [...]
+ *
+ * Config options are mean:
+ * 'alignLeft'	=> Debug toolbar will be aligned to the top left corner of browser window
+ * 'opaque'		=> Makes debug toolbar almost invisible when it's minimized
+ * 'runInDebug'	=> Show debug toolbar only if Yii application running in DEBUG MODE (see index.php for details)
+ * 'fixedPos'	=> Makes debug toolbar sticky with browser window, not document!
+ * 'collapsed'	=> Show debug toolbar minimized by default.
+ */
+
+ //TODO: Need more comments (rus: Нужно больше комментариев к коду)
+
+/**
+ * Helper class for array dumping.
+ */
+class arrayDumper
+{
+
+	private static $_objects;
+	private static $_output;
+	private static $_depth;
+
+	/**
+	 * Displays a variable.
+	 * This method achieves the similar functionality as var_dump and print_r
+	 * but is more robust when handling complex objects such as Yii controllers.
+	 * @param mixed variable to be dumped
+	 * @param integer maximum depth that the dumper should go into the variable. Defaults to 10.
+	 * @param boolean whether the result should be syntax-highlighted
+	 */
+	public static function dump($var,$depth=10,$highlight=false, $yamlStyle = false)
+	{
+		return $yamlStyle == false ? CVarDumper::dumpAsString($var, $depth, $highlight) : self::dumpAsString($var,$depth,$highlight);
+	}
+
+	/**
+	 * Dumps a variable in terms of a string.
+	 * This method achieves the similar functionality as var_dump and print_r
+	 * but is more robust when handling complex objects such as Yii controllers.
+	 * @param mixed variable to be dumped
+	 * @param integer maximum depth that the dumper should go into the variable. Defaults to 10.
+	 * @param boolean whether the result should be syntax-highlighted
+	 * @return string the string representation of the variable
+	 */
+	public static function dumpAsString($var,$depth=10,$highlight=false)
+	{
+		self::$_output='';
+		self::$_objects=array();
+		self::$_depth=$depth;
+		self::dumpInternal($var,0);
+		if($highlight)
+		{
+			$result=highlight_string("<?php\n".self::$_output,true);
+			self::$_output=preg_replace('/&lt;\\?php<br \\/>/','',$result,1);
+		}
+		return self::$_output;
+	}
+
+	private static function dumpInternal($var,$level)
+	{
+		switch(gettype($var))
+		{
+			case 'boolean':
+				self::$_output.=$var?'true':'false';
+				break;
+			case 'integer':
+				self::$_output.="$var";
+				break;
+			case 'double':
+				self::$_output.="$var";
+				break;
+			case 'string':
+				self::$_output.="'$var'";
+				break;
+			case 'resource':
+				self::$_output.='{resource}';
+				break;
+			case 'NULL':
+				self::$_output.="null";
+				break;
+			case 'unknown type':
+				self::$_output.='{unknown}';
+				break;
+			case 'array':
+				if(self::$_depth<=$level)
+					self::$_output.='array(...)';
+				else if(empty($var))
+					self::$_output.='{ }';
+				else
+				{
+					$keys=array_keys($var);
+					$spaces=str_repeat(' ',$level*2);
+					self::$_output.=$spaces.'';
+					foreach($keys as $key)
+					{
+						self::$_output.=($level == 0 ? '' : "\n").$spaces."  $key: ";
+						self::$_output.=self::dumpInternal($var[$key],$level+1);
+						self::$_output.=($level == 0 ? "\n" : '');
+					}
+					self::$_output.="";
+				}
+				break;
+			case 'object':
+				if(($id=array_search($var,self::$_objects,true))!==false)
+					self::$_output.=get_class($var).'#'.($id+1).'(...)';
+				else if(self::$_depth<=$level)
+					self::$_output.=get_class($var).'(...)';
+				else
+				{
+					$id=array_push(self::$_objects,$var);
+					$className=get_class($var);
+					$members=(array)$var;
+					$keys=array_keys($members);
+					$spaces=str_repeat(' ',$level*2);
+					self::$_output.="$className ID:#$id";//\n".$spaces.'(';
+					foreach($keys as $key)
+					{
+						$keyDisplay=strtr(trim($key),array("\0"=>'->'));
+						self::$_output.="\n".$spaces."  $keyDisplay: ";
+						self::$_output.=self::dumpInternal($members[$key],$level+1);
+					}
+					self::$_output.="\n".$spaces.')';
+				}
+				break;
+				default:
+					 self::$_output.="\n".$spaces.'~'.$var;
+		}
+	}
+}
+
+/**
+ * Render debug panel to document using view and configuration parameters
+ */
+class yiiDebugPanel
+{
+	public function render($items = array(), $config = array())
+	{
+		$msg = "Run rendering...\n";
+		$alignLeft = isset($config['alignLeft']) ? true : false;
+		$opaque = isset($config['opaque']) ? true : false;;
+		$fixedPos = isset($config['fixedPos']) ? true : false;
+		$collapsed = isset($config['collapsed']) ? true : false;;
+
+		$viewFile=dirname(__FILE__).DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'debugPanel.php';
+		include(Yii::app()->findLocalizedFile($viewFile,'en'));
+	}
+}
+
+/**
+ * Parent class for the classes may be used on DebugPanel.
+ * It have basic grid render and configuration functionality .
+ */
+class yiiDebugClass
+{
+	protected static $_config = array();
+
+	public static function timestampToTime($timestamp)
+	{
+		return date('H:i:s.',$timestamp).(int)(($timestamp-(int)$timestamp)*1000000);
+	}
+
+	public static function render($items)
+	{
+		$result = '';
+		$odd = true;
+		foreach ($items as $item)
+		{
+			list($message, $level, $category, $timestamp) = $item;
+			$message = CHtml::encode($message);
+			$time=yiiDebugTrace::timestampToTime($timestamp);
+			$odd = !$odd;
+
+			$result .= '<tr'.($odd ? ' class="odd"' : '').'><td>'.$time.'</td><td>'.$level.'</td><td>'.$category.'</td><td>'.$message.'</td></tr>';
+		}
+
+		if ($result !== '') $result = '<tbody>'.$result.'</tbody>';
+
+		$result = '<table><thead><tr><th>Time</th><th>Level</th><th>Category</th><th width="100%">Message</th></tr></thead>'.$result.'</table>';
+
+		return $result;
+	}
+
+	public static function getInfo($data, $config = null)
+	{
+		if (!is_null($config))
+		{
+			self::$_config = $config;
+		}
+	}
+}
+
+class yiiDebugDB extends yiiDebugClass
+{
+	public static function getInfo($data, $config = null)
+	{
+		parent::getInfo($data);
+		$result = array();
+		$result['panelTitle'] = 'Database Queries';
+		$count = 0;
+		$items = array();
+		foreach ($data as $row)
+		{
+			if (substr($row[2],0,9) == 'system.db')
+			{
+				$items[] = $row;
+				if ($row[2] == 'system.db.CDbCommand') $count++;
+			}
+		}
+
+		if (count($items) > 0) $result['content'] = yiiDebugTrace::render($items);
+
+		$result['title'] = 'DB Query: '.$count;
+
+		return $result;
+	}
+}
+
+class yiiDebugTrace extends yiiDebugClass
+{
+	public static function getInfo($data, $config = null)
+	{
+		parent::getInfo($data);
+		$result = array();
+		$result['title'] = 'App Log';
+		$result['panelTitle'] = 'Application Log';
+		$items = array();
+		foreach ($data as $row)
+		{
+			if (substr($row[2],0,9) != 'system.db')
+			$items[] = $row;
+		}
+
+		if (count($items) > 0) $result['content'] = yiiDebugTrace::render($items);
+
+		return $result;
+	}
+}
+
+class yiiDebugTime extends yiiDebugClass
+{
+	public static function getInfo($data, $config = null)
+	{
+		parent::getInfo($data);
+		$result = array();
+		$result['title'] = 'Time: '.(round(Yii::getLogger()->getExecutionTime(),3));
+
+		return $result;
+	}
+}
+
+class yiiDebugMem extends yiiDebugClass
+{
+	public static function getInfo($data, $config = null)
+	{
+		parent::getInfo($data);
+		$result = array();
+		//round it for two digits after point
+		$result['title'] = 'Memory: '.(round(Yii::getLogger()->getMemoryUsage() / 1024, 2)).'Kb';
+
+		return $result;
+	}
+}
+
+class yiiDebugConfig extends yiiDebugClass
+{
+	public static $yamlStyle = false;
+
+	public static function getHeadInfo()
+	{
+		$result = '';
+
+		$config = array(
+			'debug'			=> (DEFINED('YII_DEBUG') && YII_DEBUG != false) ? 'on' : 'off',
+			'xdebug'		=> extension_loaded('xdebug') ? 'on' : 'off',
+			'tokenizer'		=> function_exists('token_get_all') ? 'on' : 'off',
+			'eaccelerator'	=> extension_loaded('eaccelerator') && ini_get('eaccelerator.enable') ? 'on' : 'off',
+			'apc'			=> extension_loaded('apc') && ini_get('apc.enabled') ? 'on' : 'off',
+			'xcache'		=> extension_loaded('xcache') && ini_get('xcache.cacher') ? 'on' : 'off',
+		);
+
+		$result = '<ul class="yiiDebugInfoInline">';
+		foreach ($config as $key => $value)
+		{
+			$result .= '<li class="is'.$value.'">'.$key.'</li>';
+		}
+		$result .= '</ul>';
+
+		return $result;
+	}
+
+	public static function sessionAsArray()
+	{
+		if (isset($_SESSION))
+		{
+			$phpSession = array();
+			$sessKeyLen = null;
+			foreach ($_SESSION as $key=>$value)
+			{
+				if (is_null($sessKeyLen))
+				{
+					$values['PHP']['Key'] = substr($key, 1, strpos($key, '_')-1);
+					$sessKeyLen = strlen($values['PHP']['Key'])+2;
+				}
+				$phpSession[substr($key, $sessKeyLen)] = $value;
+			}
+			$values['PHP']['Data'] = $phpSession;
+		}
+		if (isset($_COOKIE)) $values['Cookie'] = $_COOKIE;
+		$values['Yii'] = Yii::app()->session;
+		return $values;
+	}
+
+	public static function globalsAsArray()
+	{
+		$values = array();
+		foreach (array('server', 'files', 'env') as $name)
+		{
+			if (!isset($GLOBALS['_'.strtoupper($name)])) continue;
+
+			$values[$name] = array();
+			foreach ($GLOBALS['_'.strtoupper($name)] as $key => $value)
+			{
+				$values[$name][$key] = $value;
+			}
+			ksort($values[$name]);
+		}
+
+		ksort($values);
+
+		return $values;
+	}
+
+	public static function phpInfoAsArray()
+	{
+		$values = array(
+			'php'			=> phpversion(),
+			'os'			=> php_uname(),
+			'extensions'	=> get_loaded_extensions(),
+		);
+
+		// assign extension version
+		if ($values['extensions'])
+		{
+			foreach($values['extensions'] as $key => $extension)
+			{
+				$values['extensions'][$key] = phpversion($extension) ? sprintf('%s (%s)', $extension, phpversion($extension)) : $extension;
+			}
+		}
+
+		return $values;
+	}
+
+	public static function requestAsArray()
+	{
+		$values = array();
+		if (isset($_GET)) $values['Get'] = $_GET;
+		if (isset($_POST)) $values['Get'] = $_POST;
+		$values['Yii'] = Yii::app()->request;
+		return $values;
+	}
+
+	public static function yiiAppAsArray()
+	{
+		$result = Yii::app();
+		return Yii::app();
+	}
+
+	protected static function formatArrayAsHtml($id, $values, $highlight = false)
+	{
+		$id = ucfirst(strtolower($id));
+
+		return '
+		<div style="text-align: left" class="yiiDebugInfoList">
+		<h2> <a href="#" onclick="yiiWebDebugToggleVisible(\'yiiWDCFG'.$id.'\'); return false;">+</a>'.$id.'</h2>'.
+		//'<div id="yiiWDCFG'.$id.'" style="display: none;"><pre>' .($formatted ? $values : arrayDumper::dump(arrayDumper::removeObjects($values),$highlight)) . '</pre></div></div>';
+		'<div id="yiiWDCFG'.$id.'" style="display: none;"><pre>' .arrayDumper::dump($values, 10, $highlight, isset(self::$_config['yamlStyle'])) . '</pre></div></div>';
+	}
+
+	public static function getInfo($data, $config = null)
+	{
+		parent::getInfo($data, $config);
+		$result = array();
+		$result['title'] = 'Yii ver: '.(Yii::getVersion());
+		$result['headinfo'] = self::getHeadInfo();
+		$result['panelTitle'] = 'Configuration';
+		$result['content'] = self::formatArrayAsHtml('globals', self::globalsAsArray(),true);
+		$result['content'] .= self::formatArrayAsHtml('session', self::sessionAsArray(), true);
+		$result['content'] .= self::formatArrayAsHtml('php', self::phpInfoAsArray(), true);
+		$result['content'] .= self::formatArrayAsHtml('request', self::requestAsArray(), true);
+		$result['content'] .= self::formatArrayAsHtml('Yii::app()', self::YiiAppAsArray() , true);
+		$result['content'] .= self::formatArrayAsHtml('WebdebugToolbar', self::$_config, true);
+
+		return $result;
+	}
+}
+
+/**
+ * Main class for using inside an Yii application
+ *
+ * It processes the logs of running instance of application
+ * and renders self output to the end of server output (after </html> tag of document).
+ */
+class XWebDebugRouter extends CLogRoute
+{
+	public $config = '';
+
+	public function collectLogs($logger, $processLogs = false)
+	{
+		$logs=$logger->getLogs($this->levels,$this->categories);
+		if(empty($logs)) $logs = array();
+		$this->processLogs($logs);
+	}
+
+	public function processLogs($logs)
+	{
+		$app=Yii::app();
+		$config = array();
+		foreach (explode(',', $this->config) as $value)
+		{
+			$value = trim($value);
+			$config[$value] = true;
+		}
+
+		//Checking for an AJAX Requests
+		if(!($app instanceof CWebApplication) || $app->getRequest()->getIsAjaxRequest()) return;
+
+		//Checking for an DEBUG mode of running app
+		if (isset($config['runInDebug']) && (!DEFINED('YII_DEBUG') || YII_DEBUG == false)) return;
+
+		$items = array();
+
+		$items[] = yiiDebugConfig::getInfo($logs, $config);
+		$items[] = yiiDebugMem::getInfo($logs);
+		$items[] = yiiDebugTime::getInfo($logs);
+		$items[] = yiiDebugDB::getInfo($logs);
+		$items[] = yiiDebugTrace::getInfo($logs);
+
+		$panel = new yiiDebugPanel();
+		$panel->render($items, $config);
+	}
+}
---
+++ views/debugPanel.php
@@ -0,0 +1,278 @@
+<style type="text/css">
+	#yiiWebDebugToolbar {
+		<?php if ($fixedPos): ?>
+		position: fixed;
+		<?php else: ?>
+		position: absolute;
+		<?php endif ?>
+		
+		<?php if ($alignLeft): ?>
+		float: left;
+		left: 0;
+		<?php else: ?>
+		float: right;
+		right: 0;
+		<?php endif ?>
+		
+		top: 0;
+		height: 16px;
+		background-color: #eef;
+		border: solid 1px #000;
+		padding: 1px;
+		z-index: 65536;
+		font: normal 10pt Arial, Helvetica,sans-serif;
+	}
+	
+	.yiiWebDebugOpacity {
+		opacity: 0.1;
+		filter: alpha(opacity:10);
+	}
+	
+	.yiiWebDebugOpacity:hover {
+		opacity: 1;
+		filter: alpha(opacity:100)
+	}
+	
+	#yiiWebDebugToolbar ul {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+		float: left;
+	}
+	
+	#yiiWebDebugToolbar ul li {
+		float: left;
+	}
+	
+	#yiiWebDebugToolbar .yiiLink {
+		background-color: #000;
+		color: #fff;
+		padding-left: 5px;
+		padding-right: 5px;
+		text-decoration: none;
+		font-weight: bold;
+	}
+	
+	#yiiWebDebugToolbar .yiiLinkItem {
+		color: #444;
+	}
+	
+	#yiiWebDebugToolbar .yiiLinkItem:hover {
+		text-decoration: none;
+	}
+	
+	ul#yiiWebDebugToolbarItems {
+		<?php if ($alignLeft): ?>
+		margin-right: 5px;
+		<?php else: ?>
+		margin-left: 5px;
+		<?php endif ?>
+	}
+	
+	ul#yiiWebDebugToolbarItems li {
+		margin-top: 1px;
+		font-size: 11px;
+		font-weight: bold;
+	}
+	
+	#yiiWebDebugPanel {
+		background-color: #eee;
+		position: absolute;
+		width: 100%;
+		top: 0;
+		z-index:65535;
+		border: solid 1px #888;
+	}
+	
+	#yiiWebDebugPanel .panelHeadInfo {}
+	
+	#yiiWebDebugPanel .gridContainer {
+		padding: 6px;
+	}
+	
+	#yiiWebDebugPanel .panelTitle {
+		background-color: #000;
+		color: #fff;
+		font-weight: bold;
+		text-align: center;
+	}
+	
+	#yiiWebDebugPanel .panelGrid {
+		overflow: auto;
+		padding: 1px;
+	}
+	
+	#yiiWebDebugPanel .panelGrid table {
+		width: 100%;
+		margin-top: 6px;
+		border-collapse: collapse;
+		background-color: #fff;
+	}
+	
+	#yiiWebDebugPanel .panelGrid table thead th {
+		padding-right: 5px;
+		padding-left: 5px;
+		border: solid 1px #888;
+		background-color: #ddd;
+	}
+	
+	#yiiWebDebugPanel .panelGrid table tbody tr.odd {
+		background-color: #f5f5ff;
+	}
+	
+	#yiiWebDebugPanel .panelGrid table tbody td {
+		padding-right: 5px;
+		padding-left: 5px;
+		border: solid 1px #888;
+	}
+	
+	.yiiDebugInfoInline {
+		list-style: none;
+		margin: 5px;
+		padding: 5px;
+	}
+	
+	.yiiDebugInfoInline li {
+		display: inline;
+		border: solid 1px #444;
+		padding: 3px;
+		margin: 1px;
+		background-color: #bbb;
+	}
+	
+	.yiiDebugInfoInline li.ison {
+		background-color: #4f4;
+	}
+	
+	.yiiDebugInfoList {}
+	
+	.yiiDebugInfoList h2 {
+		font-size: 16px;
+		margin: 0;
+		border-bottom: solid 1px #bbb;
+	}
+	
+	.yiiDebugInfoList h2 a {
+		text-decoration: none;
+		color: #000;
+	}
+	
+	.yiiDebugInfoList div {
+		border-bottom: solid 1px #bbb;
+	}
+
+	.yiiDebugInfoList div pre {
+		margin: 0px;
+		padding: 0px;
+	}
+
+	.yiiDebugInfoList div pre code {
+		margin: 0px;
+		padding: 0px;
+	}
+
+	.yiiDebugInfoList div pre code span {
+		font-size: 1px;
+	}
+
+	.yiiDebugInfoList div pre code span span {
+		font-size: 10pt;
+	}
+</style>
+
+<div id="yiiWebDebugToolbar" onmouseover="yiiDebugMouse(true);" onmouseout="yiiDebugMouse(false);">
+	<?php if (!$alignLeft): ?>
+	<ul>
+		<li><a href="#" class="yiiLink" onClick="yiiWebDebugToggle('yiiWebDebugToolbarItems');">Yii</a></li>
+	</ul>
+	<?php endif ?>
+	
+	<ul id="yiiWebDebugToolbarItems">
+		<?php $index = 0; foreach ($items as $item): ?>
+		<li>[&nbsp;
+			<?php echo (isset($item['content']) && !is_null($item['content'])) ? '<a href="#" class="yiiLinkItem" onClick="yiiWebDebugToggle(\'__yiiWDP'.$index.'\');">'.$item['title'].'</a>' : $item['title'] ?>
+			&nbsp;]
+		</li>
+		<?php if (isset($item['content']) && !is_null($item['content'])) $index++; endforeach ?>
+	</ul>
+	
+	<?php if ($alignLeft): ?>
+	<ul>
+		<li><a href="#" class="yiiLink" onClick="yiiWebDebugToggle('yiiWebDebugToolbarItems');">Yii</a></li>
+	</ul>
+	<?php endif ?>
+</div>
+
+<div id="yiiWebDebugPanel">
+	<?php
+	$index = 0;
+	foreach ($items as $item): if (!isset($item['content']) || is_null($item['content'])) continue; ?>
+	
+	<div id="__yiiWDP<?php echo $index ?>" style="display: none">
+		<div class="panelHeadInfo">
+			<?php if ($alignLeft) echo "<br/>" ?> <?php echo (isset($item['headinfo']) && !is_null($item['headinfo'])) ? $item['headinfo'] : '<br/><br/>' ?>
+		</div>
+		
+		<center>
+			<div class="gridContainer">
+				<div class="panelTitle">
+					<?php if (isset($item['panelTitle']) && !is_null($item['panelTitle'])) echo $item['panelTitle'] ?>
+				</div>
+				
+				<div class="panelGrid" id="panelGridH__yiiWDP<?php echo $index ?>">
+					<div id="panelGH__yiiWDP<?php echo $index ?>">
+						<?php echo $item['content'] ?>
+					</div>
+				</div>
+			</div>
+		</center>
+	</div>
+	
+	<?php
+	$index++;
+	endforeach; ?>
+</div>
+
+<script type="text/javascript">
+	var _curPanel = '';
+	var panelMaxHeight = 100;
+	
+	//Selector function
+	function _$(element) { return document.getElementById(element); }
+	
+	function yiiWebDebugToggleVisible(element) { _$(element).style.display = (_$(element).style.display == 'block') ? 'none' : 'block'; }
+	
+	function yiiWebDebugToggle(element)
+	{
+		disp = _$(element).style.display == 'none' ? 'block' : 'none';
+		if (element == 'yiiWebDebugToolbarItems')
+		{
+			if (_curPanel != '' && disp == 'none') _$(_curPanel).style.display = 'none';
+			<?php if ($opaque): ?>
+			_$('yiiWebDebugToolbar').className = (disp == 'none') ? 'yiiWebDebugOpacity' : '';
+			<?php endif ?>
+		}
+		else
+		{
+			if (_curPanel != '') _$(_curPanel).style.display = 'none';
+			_curPanel = element;
+		}
+		
+		_$(element).style.display = disp;
+		
+		return false;
+	}
+	
+	function yiiDebugMouse(over)
+	{
+		<?php if ($opaque): ?>
+		_$('yiiWebDebugToolbar').className = (!over && _$('yiiWebDebugToolbarItems').style.display == 'none') ? 'yiiWebDebugOpacity' : '';
+		<?php endif ?>
+		
+		return false;
+	}
+	
+	<?php if ($collapsed): ?>
+	yiiWebDebugToggle('yiiWebDebugToolbarItems');
+	<?php endif ?>
+</script>
